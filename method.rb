aec4c56cc035a954bf0c657bc4c8ecb6a4ac1574
shohankazi <shohankazi012@gmail.com>
2024-05-24 18:17:53 +0600

    return statement in ruby cleared

---
+++ method.rb
@@ -0,0 +1,11 @@
+# def sayhi(name,age)
+#     puts "Hello " + name
+#     puts "You are " + age.to_s + " years old"
+# end
+
+# sayhi("Asan",25)
+
+def cube(num)
+    return num * num * num
+end
+puts cube(3)
