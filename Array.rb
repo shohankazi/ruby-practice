8a5f432697d5f050b25eea837d386cfddec76c76
shohankazi <shohankazi012@gmail.com>
2024-05-24 12:41:53 +0600

    Ruby Arrays completed

---
+++ Array.rb
@@ -0,0 +1,11 @@
+friends = Array["Kevin", "Karen", "Oscar"]
+
+# puts friends[0]
+friends[0] = "Levin"
+# puts friends.length()
+# puts friends.include?("Karen")
+# puts friends.reverse()
+puts friends.sort()
+
+
+
