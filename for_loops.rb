bf62eaa842bb66e0b0f9e1d1d4d6bfd6afaafbbe
shohankazi <shohankazi012@gmail.com>
2024-05-25 21:35:32 +0600

    for loop concept cleared

---
+++ for_loops.rb
@@ -0,0 +1,25 @@
+friends = ["Kevin","Karen", "Oscar","Angela","Andy"]
+
+# First type of for loop
+
+for friend in friends
+    puts friend
+end
+
+# Second type of for loop
+
+friends.each do |friend|
+    puts friend
+end
+
+# Third type of for loop
+
+for index in 0..5
+    puts index
+end
+
+# Fourth type of for loop
+
+6.times do |index|
+    puts index
+end
