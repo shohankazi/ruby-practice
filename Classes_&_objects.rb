3768cfc1ac1897ed1f73fe14d4e4a0504e3c94d5
shohankazi <shohankazi012@gmail.com>
2024-05-26 16:35:50 +0600

    Classes and objects in ruby

---
+++ Classes_&_objects.rb
@@ -0,0 +1,18 @@
+class Book
+    attr_accessor :title, :author, :pages
+end
+
+book1 = Book.new()
+book1.title = "Harry Potter"
+book1.author = "JK Rowling"
+book1.pages = 400
+
+puts book1.title
+
+
+book2 = Book.new()
+book2.title = "ICT"
+book2.author = "Shohan Kazi"
+book2.pages = 200
+
+puts book2.author
