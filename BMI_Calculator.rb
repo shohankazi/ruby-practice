336023485f46e864053cdf355bc3bb434425e806
shohankazi <shohankazi012@gmail.com>
2024-05-27 00:12:04 +0600

    BMI Calculator made in Rubyy

---
+++ BMI_Calculator.rb
@@ -0,0 +1,17 @@
+puts "Body Mass Index (BMI) Categories"
+puts "-Severe Thinness < 16"
+puts "-Moderate Thinness < 16 - 17"
+puts "-Mild Thinness < 17 - 18.5"
+puts "-Normal 18.5 - 25"
+puts "-Overweight 25-30"
+puts "-Obese Class I 30-35"
+puts "-Obese Class II 35-40"
+puts "-Obese Class III > 40"
+puts
+puts "Body Mass Index (BMI) Calculator"
+puts "Height(cm)"
+height = gets.chomp.to_f
+puts "Weight(kg): "
+weight = gets.chomp.to_f
+bmi = weight/(height/100)**2
+puts "BMI = #{bmi}"
