34023abeb68c397be1903437ba791f5c054d2570
shohankazi <shohankazi012@gmail.com>
2024-05-26 17:05:48 +0600

    Object methods

--- Classes_&_objects.rb
+++ Classes_&_objects.rb
@@ -1,17 +1,36 @@
-class Book
-    attr_accessor :title, :author, :pages
-    def initialize(title, author, pages)
-        @title = title
-        @author = author
-        @pages = pages
+class Student
+    attr_accessor :name, :major, :gpa   
+    def initialize(name, major, gpa)
+        @name = name
+        @major = major
+        @gpa = gpa
+end
+    def has_honours 
+        if @gpa >= 3.5
+            return true    
+        end
+        return false
     end
 end
 
+Student1 = Student.new("Jim", "Business", 4.6)
+puts Student1.has_honours
 
-book1 = Book.new("Harry Potter","JK Rowling", 300)
-puts book1.title
-puts book1.author
-puts book1.pages
+
+# class Book
+#     attr_accessor :title, :author, :pages
+#     def initialize(title, author, pages)
+#         @title = title
+#         @author = author
+#         @pages = pages
+#     end
+# end
+
+
+# book1 = Book.new("Harry Potter","JK Rowling", 300)
+# puts book1.title
+# puts book1.author
+# puts book1.pages
 
 
 
